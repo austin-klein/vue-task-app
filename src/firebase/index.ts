22f3083b3a48ed4c05310cf3684b56368c17d293
Austin Klein <austinklein34@gmail.com>
2022-11-11 12:03:10 -0700

    first commit

---
+++ src/firebase/index.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCv0VQaK8LnNlqBT1DB12lxwQJNw1qfgv8",
+  authDomain: "vue-task-app-1d498.firebaseapp.com",
+  projectId: "vue-task-app-1d498",
+  storageBucket: "vue-task-app-1d498.appspot.com",
+  messagingSenderId: "535984018208",
+  appId: "1:535984018208:web:549ea1a9470151ae503d45",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
